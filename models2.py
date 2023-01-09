d80f9ca92f07978cccc83864094c104b3355b1d8
1shingo1 <shin2312@icloud.com>
2023-01-09 10:36:02 +0400

    add class player

--- models2.py
+++ models2.py
@@ -6,7 +6,30 @@ class Card:
     def use(self):
         pass
 
+
 class Deck:
     def __init__(self, name):
         self.name = name
         self.card = []
+
+    def shuffle(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+
+class Player:
+    def __init__(self,name, level, hand):
+        self.hand = hand
+        self.level = level
+        self.name = name
+
+    def start(self):
+        pass
