25fee4459391e2f8e0f221136134cd1c37f0b360
1shingo1 <shin2312@icloud.com>
2023-01-11 23:10:15 +0400

    new build

--- buid.py
+++ buid.py
@@ -5,20 +5,11 @@ from models2 import MonsterCard, LootCard, Deck, Player
 pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
-monster_cards = [MonsterCard("Goblin", 1), MonsterCard("Troll", 3), MonsterCard("Dragon", 5), MonsterCard("Orc", 2), MonsterCard("Undead", 1)]
-monster_deck = Deck("Monsters", monster_cards)
-loot_cards = [LootCard("Helm", 2), LootCard("Potion", 1), LootCard("Shield", 3), LootCard("Sword", 2),LootCard("Boots", 1)]
-loot_deck = Deck("Loot", loot_cards)
-monster_deck.shuffle()
-loot_deck.shuffle()
-player = Player("Shingo", monster_deck, loot_deck)
-player.start()
 
 # Define some colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# Create some monster and loot cards and decks
 monster_cards = [MonsterCard("Goblin", 1), MonsterCard("Troll", 3),
                  MonsterCard("Dragon", 5), MonsterCard("Orc", 2),
                  MonsterCard("Undead", 1)]
@@ -28,30 +19,18 @@ loot_cards = [LootCard("Helm", 2), LootCard("Potion", 1),
               LootCard("Shield", 3), LootCard("Sword", 2),
               LootCard("Boots", 1)]
 loot_deck = Deck("Loot", loot_cards)
-
-# Shuffle the decks
 monster_deck.shuffle()
 loot_deck.shuffle()
-
-# Create a player
 player = Player("Shingo", monster_deck, loot_deck)
 player.start()
-
-# Create a screen
 screen = pygame.display.set_mode((800, 640))
 pygame.display.set_caption("Monster Battle")
-
-# Rectangle for the player field
 player_rect = pygame.Rect(10, 10, 300, 100)
-
-# Rectangle for the hand field
 hand_rect = pygame.Rect(10, 125, 300, 100)
-
-# Rectangle for the used cards field
 used_cards_rect = pygame.Rect(10, 240, 300, 100)
-
-# Rectangle for the battle field
 battle_rect = pygame.Rect(320, 10, 300, 330)
+
+
 
 def render_text(text, x, y, font_size):
     """ Helper function to render text on the screen"""
@@ -59,12 +38,14 @@ def render_text(text, x, y, font_size):
     text = font.render(text, True, BLACK)
     screen.blit(text, (x, y))
 
+
 def player_field():
     """ Function for rendering the player field"""
     pygame.draw.rect(screen, WHITE, player_rect)
     render_text("Игрок:", 20, 20, 30)
     render_text(player.name, 20, 40, 30)
     render_text("Level: " + str(player.level), 20, 60, 25)
+
 
 def hand_field():
     """ Function for rendering the hand field"""
@@ -74,6 +55,7 @@ def hand_field():
     for i, card in enumerate(player.hand):
         card_text = str(card)
         render_text(card_text, 20, 145 + i * card_spacing, 25)
+
 
 def using_card():
     """ Function for using cards"""
@@ -99,11 +81,16 @@ def using_card():
 
 
 def battle_field():
-    monster_name, monster_level, monster_obj = player.battle()
     pygame.draw.rect(screen, WHITE, battle_rect)
-    render_text("Поле активного монстра:", 320, 20, 30)
-    render_text("Имя монстра: " + monster_name, 320, 40, 25)
-    render_text("Уровень монстра: " + str(monster_level), 320, 60, 25)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if battle_rect.collidepoint(event.pos):
+                monster_name, monster_level, monster_obj = player.battle()
+                render_text("Поле активного монстра:", 320, 20, 30)
+                render_text("Имя монстра: " + monster_name, 320, 40, 25)
+                render_text("Уровень монстра: " + str(monster_level), 320, 60, 25)
+
+
 
 def draw_main_menu():
     # Clear the screen
@@ -119,17 +106,23 @@ def draw_main_menu():
     # Draw play button
     global play_button
     play_button = pygame.Rect(300, 200, 200, 50)
-    pygame.draw.rect(screen, (255, 255, 255), play_button)
+    pygame.draw.rectpygame.draw.rect(screen, (255, 255, 255), play_button)
     play_text = "Play"
     play_font = pygame.font.Font(None, 30)
     play_text_surface = play_font.render(play_text, True, (0, 0, 0))
     play_text_rect = play_text_surface.get_rect(center=play_button.center)
     screen.blit(play_text_surface, play_text_rect)
 
+
+def result():
+    pygame.draw.rect(screen, WHITE, battle_rect)
+    render_text('Результат:', 20, 400, 25)
+    render_text(player.text, 20, 440, 25)
+
+
 def game_loop():
     """ Main game loop"""
-    running = True
-    while running:
+    while True:
         # Draw main menu here
         player_field()
         hand_field()
@@ -139,10 +132,9 @@ def game_loop():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if hand_rect.collidepoint(event.pos):
                     card_rects = []
                     card_spacing = 30
@@ -151,16 +143,12 @@ def game_loop():
                         card_rect = pygame.Rect(20, 130 + i * card_spacing, 200, 30)
                         card_rects.append(card_rect)
                         if card_rect.collidepoint(event.pos):
-                            selected_card = card
                             player.use_card(index)
                         index += 1
-                    pass
-                elif battle_rect.collidepoint(event.pos):
-                    monster_name, monster_level, monster_obj = player.battle()
-                    battle_field()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player.fight(monster_obj)
+                    result()
 
 
 game_loop()
-
-
-
